a92b58364e66b3a18e3971f39f720a60eca59f93
DonalDuck004 <burlarenato2012@gmail.com>
2023-04-24 22:58:30 +0200

    Bug fixes, other improvements, initials logs

---
+++ ClasseVivaWPF/Utils/Logs/LogLevel.cs
@@ -0,0 +1,15 @@
+﻿#define LOG_ENABLED
+
+
+namespace ClasseVivaWPF.Utils.Logs
+{
+    public enum LogLevel
+    {
+        TRACE,
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR,
+        FATAL
+    }
+}
---
+++ ClasseVivaWPF/Utils/Logs/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseVivaWPF.Utils.Logs
+{
+    public class Logger
+    {
+        public static Logger INSTANCE { get; } = new();
+        public Stream OutStrem { get; set; }
+
+        private Logger() { 
+
+        }
+
+    }
+}
